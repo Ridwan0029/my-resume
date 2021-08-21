77113c313dac42cb36f2a4db37cfcd8a37553eb5
Ridwan <ridwanraji4@gmail.com>
2021-08-21 21:42:54 +0100

    connect to database

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 
-//mongoose.connect('mongodb://localhost/my-resume');
-mongoose.connect('mongo "mongodb+srv://cluster0.t86kn.mongodb.net/test" --username Kayode');
-//mongo "mongodb+srv://cluster0.t86kn.mongodb.net/myFirstDatabase" --username Kayode
-// mongodb+srv://Ridwan:Adrenalin0029@cluster0.t86kn.mongodb.net/clusters?retryWrites=true&w=majority
+
+const mongoose = require('mongoose')
+
+const url = `mongodb+srv://ridwan:Adrenalin0029@cluster0.t86kn.mongodb.net/test?retryWrites=true&w=majority`;
+
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
@@ -70,6 +85,77 @@ app.post('/myresume', function(req, res){
 app.get('/myresume/new', function(req, res){
     res.render('new.ejs');
 });
+
+
+
+
+// //mongoose.connect('mongodb://localhost/my-resume');
+// mongoose.connect('mongo "mongodb+srv://cluster0.t86kn.mongodb.net/test" --username Kayode');
+// //mongo "mongodb+srv://cluster0.t86kn.mongodb.net/myFirstDatabase" --username Kayode
+// // mongodb+srv://Ridwan:Adrenalin0029@cluster0.t86kn.mongodb.net/clusters?retryWrites=true&w=majority
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.set('view engine', 'ejs');
+
+// //SCHEMA SETUP
+// var myresumeSchema = new mongoose.Schema({
+//     firstName: String,
+//     lastName: String
+// });
+
+// var Myresume = mongoose.model('Myresume', myresumeSchema )
+// // Myresume.create(
+// //     {
+// //         firstName: 'Rasheedah',
+// //         lastName: 'zubayr'
+// //     }, function(err, resume){
+// //         if(err){
+// //             console.log(err);
+// //         } else {
+// //             console.log('NEWLY CREATED COMMENT: ')
+// //             console.log(resume);
+// //         }
+// //     }
+// // );
+
+// // var data = [
+// //     {firstName: 'Ridwan', lastName: 'Raji'}
+// // ]
+
+// app.get('/', function(req, res){
+//     res.render('landing');
+// });
+
+// app.get('/myresume', function(req, res){
+//     //Get all comments from DB
+//     Myresume.find({}, function(err, allResume){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             res.render('myresume', {data: allResume});
+//         }
+//     });
+// });
+
+// app.post('/myresume', function(req, res){
+//     //get data from form and add to data array
+//     var firstName = req.body.nameOne
+//     var lastName = req.body.nameTwo
+//     var user =  {firstName: firstName, lastName: lastName}
+//     //Create a new comment and save to DB
+//     Myresume.create(user, function(err, newlyCreated){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             //redirect back to campgrounds page
+//             res.redirect('/myresume')
+//         }
+//     });
+    
+// });
+
+// app.get('/myresume/new', function(req, res){
+//     res.render('new.ejs');
+// });
 
 app.listen(process.env.PORT || 3000, function(){
     console.log('resume-builder server has started!');
